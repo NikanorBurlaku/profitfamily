b2ca9321ac155e140de6dde9d92cc57abcc96c4f
Nikanor <nicanorburlacu@gmail.com>
2023-04-13 12:24:26 +0300

    finish

--- send.php
+++ send.php
@@ -5,20 +5,27 @@ require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
 // Переменные, которые отправляет пользователь
+
 $name = $_POST['name'];
 $phone = $_POST['phone'];
-$text = $_POST['text'];
-$file = $_FILES['file'];
+$telegram = $_POST['telegram'];
+$link = $_POST['link'];
+$info = $_POST['info'];
 
 // Формирование самого письма
-$title = "Заголовок письма";
+$title = "Отклик с сайта ProfiFamily";
 $body = "
-<h2>Новое письмо</h2>
+<h2>Отклик с сайта ProfiFamily</h2>
 <b>Имя:</b> $name<br>
-<b>Почта:</b> $phone<br><br>
-<b>Сообщение:</b><br>$text
+<b>Номер телефона:</b> $phone<br>
 ";
+if(!empty($telegram)) $body.= "<b>Телеграм:</b> $telegram<br>";
+if(!empty($link)) $body.= "<b>Ссылка на резюме:</b> $link<br>";
+if(!empty($info)) $body.= "<b>Дополнительная информация:</b> $info<br>";
 
+$body .= '<br><br>';
+
+echo $body;
 // Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
@@ -27,31 +34,31 @@ try {
     $mail->SMTPAuth   = true;
     //$mail->SMTPDebug = 2;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+// Настройки вашей почты
+$mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+$mail->Username   = 'example@gmail.com'; // Логин на почте
+$mail->Password   = '******'; // Пароль на почте
+$mail->SMTPSecure = 'ssl';
+$mail->Port       = 465;
+$mail->setFrom('mail@gmail.com', 'Отклик с сайта'); // Адрес самой почты и имя отправителя
 
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-    $mail->Username   = ''; // Логин на почте
-    $mail->Password   = ''; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('mail@yandex.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
+// Получатель письма
+$mail->addAddress('example@gmail.com');  
 
-    // Получатель письма
-    $mail->addAddress('nicanorburlacu@gmail.com');  
-
-    // Прикрипление файлов к письму
-if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-        $filename = $file['name'][$ct];
-        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-            $mail->addAttachment($uploadfile, $filename);
-            $rfile[] = "Файл $filename прикреплён";
-        } else {
-            $rfile[] = "Не удалось прикрепить файл $filename";
-        }
-    }   
+// Прикрипление файлов к письму
+$file = $_FILES['file'];
+$rfile = null;
+if (!empty($file['name'])) {
+    $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name']));
+    $filename = $file['name'];
+    if (move_uploaded_file($file['tmp_name'], $uploadfile)) {
+        $mail->addAttachment($uploadfile, $filename);
+        $rfile[] = "Файл $filename прикреплён";
+    } else {
+        $rfile[] = "Не удалось прикрепить файл $filename";
+    }
 }
+
 // Отправка сообщения
 $mail->isHTML(true);
 $mail->Subject = $title;
@@ -60,11 +67,14 @@ $mail->Body = $body;
 // Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
 else {$result = "error";}
+$result = 'success';
+$status = null;
 
 } catch (Exception $e) {
     $result = "error";
     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
-
 // Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status-error" => $status]);
+
+
